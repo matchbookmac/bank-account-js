1e2a8af2af58349e25ff4ead060763439d07fcb0
Ian <ian@iancmacdonald.com>
2015-06-02 10:12:05 -0700

    Add create bank account page.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,15 @@
 
 //jQuery
 
-$( document ).ready(function() {
-    console.log( "jQuery Ready" );
-
-    $("#jqtest").text('jQuery Ready');
+$(document ).ready(function() {
+  $("form#add-account").submit(function(event) {
+    event.preventDefault();
+    var name = $("input#name").val();
+    var initialDeposit = parseInt($("input#initialDeposit").val());
+    var newAccount = new BankAccount(name, initialDeposit);
+    location.href="bank-account-info-js.html";
+  });
+  $("#balance").append("Balans: " + newAccount.balance);
 });
 
 //raw js
