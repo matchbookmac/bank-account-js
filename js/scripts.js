3655f773729f02e7340ca15f97f4d1fd59c4c1ed
Ian <ian@iancmacdonald.com>
2015-06-02 10:40:48 -0700

    Add deposit/withdrawal functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,44 @@
 //jQuery
 
 $(document ).ready(function() {
+  var account;
+
+  function updateBalance() {
+    $("#balance").empty();
+    $("#balance").append("Balans: " + (account.balance).toString());
+  }
+
+  function clearForms() {
+    $("form#add-account").find("input").val("");
+    $("form#deposit").find("input").val("");
+  }
+
   $("form#add-account").submit(function(event) {
     event.preventDefault();
     var name = $("input#name").val();
     var initialDeposit = parseInt($("input#initial-deposit").val());
-    var newAccount = new BankAccount(name, initialDeposit);
-    $("#balance").append("Balans: " + (newAccount.balance).toString());
+    account = new BankAccount(name, initialDeposit);
+    updateBalance();
+    clearForms();
   });
+
+  $("form#deposit").submit(function(event) {
+    event.preventDefault();
+    var deposit = parseInt($("input#deposit-amount").val());
+    account.deposit(deposit);
+    updateBalance();
+    clearForms();
+  });
+
+  $("form#withdraw").submit(function(event) {
+    event.preventDefault();
+    var withdrawal = parseInt($("input#withdrawal-amount").val());
+    account.withdraw(withdrawal);
+    updateBalance();
+    clearForms();
+  });
+
+
 });
 
 //raw js
