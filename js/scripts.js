40dfedb5b4e60196af0657382e70b674317c6ca2
Ian <ian@iancmacdonald.com>
2015-06-02 09:41:42 -0700

    Add BankAccount constructor, and accompanying withdrawl/deposit methods

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,24 @@
 
 
 //jQuery
+
 $( document ).ready(function() {
     console.log( "jQuery Ready" );
 
-    $("#jqtest").text('jQuery Ready')
+    $("#jqtest").text('jQuery Ready');
 });
 
 //raw js
-var foo = function(bar) {
-  return false;
-};
+
+function BankAccount(name, balance) {
+  this.name = name;
+  this.balance = balance;
+}
+
+BankAccount.prototype.deposit = function(amount) {
+  this.balance += amount;
+}
+
+BankAccount.prototype.withdraw = function(amount) {
+  this.balance -= amount;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,20 @@
-describe('foo', function() {
-  it("is some function or expectation", function() {
-    expect(foo('bar')).to.equal(false);
+describe('BankAccount', function() {
+  it("creates a new BankAccount object", function() {
+    var testBankAccount = new BankAccount("Dave Blanket", 500);
+    expect(testBankAccount.name).to.equal("Dave Blanket");
+    expect(testBankAccount.balance).to.equal(500);
   });
+
+  it("deposits money into account", function() {
+    var testBankAccount = new BankAccount("Dave Blanket", 500);
+    testBankAccount.deposit(200);
+    expect(testBankAccount.balance).to.equal(700);
+  });
+
+  it("withdraws money from account", function() {
+    var testBankAccount = new BankAccount("Dave Blanket", 500);
+    testBankAccount.withdraw(200);
+    expect(testBankAccount.balance).to.equal(300);
+  });
+
 });
